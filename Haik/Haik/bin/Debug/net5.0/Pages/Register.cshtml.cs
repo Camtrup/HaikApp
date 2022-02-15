e4fe5a1c760f5ccb4c3eb49cf00694a067b75470
casperla <casperla@stud.ntnu.no>
2022-02-15 14:12:27 +0100

    #7 Check for duplicates in database

---
+++ Haik/Haik/bin/Debug/net5.0/Pages/Register.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Haik.Pages
+{
+  public class RegisterModel : PageModel
+  {
+    private readonly ILogger<RegisterModel> _logger;
+
+    public RegisterModel(ILogger<RegisterModel> logger)
+    {
+      _logger = logger;
+    }
+
+    public void OnPost()
+    {
+      var username = Request.Form["username"];
+      Console.WriteLine("Hello, World");
+    }
+  }
+}
