01ad8d7455829c862298b8c9993162b5a5e7c2b7
Thomas Myhre <Tthomas.myhre97@hotmail.com>
2022-02-21 23:02:20 +0100

    #8-Model for creating trip

---
+++ Haik/Haik/ViewModels/CreateTripModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Haik.ViewModels
+{
+    public class CreateTripModel
+    {
+        public string description { get; set; }
+        public string location { get; set; }
+        public string difficulty { get; set; }
+        public DateTime date { get; set; }
+        public string[] equipment { get; set; }
+        public float duration { get; set; }
+        public string title { get; set; }
+    }
+}
