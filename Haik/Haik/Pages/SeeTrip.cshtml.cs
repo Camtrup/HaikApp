6f2ed6da138b656384147cea3ca1f4dcd9e3faf7
Your Name <you@example.com>
2022-03-01 13:15:53 +0100

    #27 can now sign up to event

--- Haik/Haik/Pages/SeeTrip.cshtml.cs
+++ Haik/Haik/Pages/SeeTrip.cshtml.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Haik.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace Haik.Pages
 {
     public class SeeTripModel : PageModel
     {
-        public HaikDBContext context;
+        public HaikDBContext context = null;
         public List<TripDb> datalookup;
         public int id;
         public TripDb trip;
@@ -28,6 +30,8 @@ namespace Haik.Pages
             this.id = id;
             datalookup = await context.Trips.ToListAsync();
 
+            
+
             foreach (var d in datalookup)
             {
                 int temp = (int)d.Id;
@@ -39,5 +43,125 @@ namespace Haik.Pages
             }
 
         }
+
+        public IEnumerable<TripDb> trips { get; set; }
+        public IEnumerable<ApplicationUser> users { get; set; }
+
+        public async Task OnPostAsync(int id)
+        {
+
+
+
+            ViewData["id"] = id;
+            this.id = id;
+            datalookup = await context.Trips.ToListAsync();
+
+            
+
+            foreach (var d in datalookup)
+            {
+                int temp = (int)d.Id;
+
+                if (temp.ToString().Equals(id.ToString()))
+                {
+                    trip = d;
+                }
+            }
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            //trip updating
+
+            trips = await context.Trips.ToListAsync();
+            var JSONprevOccupantsString = trips.Where<TripDb>(w => w.Id == id).FirstOrDefault().JsonParticipantUids;
+
+
+                if(JSONprevOccupantsString != null) {
+
+                    var jsonPrevdeser = JsonConvert.DeserializeObject<List<string>>(JSONprevOccupantsString);
+
+
+                if (!jsonPrevdeser.Contains(userId))
+                {
+                    jsonPrevdeser.Add(userId);
+                }
+
+                    var newJson = JsonConvert.SerializeObject(jsonPrevdeser);
+
+                    trips.Where<TripDb>(w => w.Id == id).FirstOrDefault().JsonParticipantUids = newJson;
+                    context.SaveChanges();
+
+                    //TODO set button to #"meld deg av"
+
+                }
+                else
+                {
+                    var jsonPrevdeser = new List<string>();
+
+
+                if (!jsonPrevdeser.Contains(userId))
+                {
+                    jsonPrevdeser.Add(userId);
+                }
+                var newJson = JsonConvert.SerializeObject(jsonPrevdeser);
+
+                    trips.Where<TripDb>(w => w.Id == id).FirstOrDefault().JsonParticipantUids = newJson;
+                    context.SaveChanges();
+
+                    //TODO set button to #"meld deg av"
+                }
+
+
+
+                //person updating
+
+
+                users = await context.Users.ToListAsync();
+                var uJSONprevOccupantsString = users.Where<ApplicationUser>(w => w.Id == userId).FirstOrDefault().JsonParticipatedTrips;
+
+
+
+
+
+                if (uJSONprevOccupantsString != null)
+                {
+
+                    var ujsonPrevdeser = JsonConvert.DeserializeObject<List<string>>(uJSONprevOccupantsString);
+
+
+                if (!ujsonPrevdeser.Contains(id.ToString()))
+                {
+                    ujsonPrevdeser.Add(id.ToString());
+                }
+                   
+
+                    var unewJson = JsonConvert.SerializeObject(ujsonPrevdeser);
+
+                    users.Where<ApplicationUser>(w => w.Id == userId).FirstOrDefault().JsonParticipatedTrips = unewJson;
+                    context.SaveChanges();
+
+
+                }
+                else
+                {
+                    var ujsonPrevdeser = new List<string>();
+
+
+
+                if (!ujsonPrevdeser.Contains(id.ToString()))
+                {
+                    ujsonPrevdeser.Add(id.ToString());
+                }
+
+                var unewJson = JsonConvert.SerializeObject(ujsonPrevdeser);
+
+                    users.Where<ApplicationUser>(w => w.Id == userId).FirstOrDefault().JsonParticipatedTrips = unewJson;
+                    context.SaveChanges();
+
+                }  
+
+            
+        }
+
     }
 }
