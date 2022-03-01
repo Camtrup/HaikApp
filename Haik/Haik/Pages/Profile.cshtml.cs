31e7f14bb78aeb659d6e2991d02281c68881fc63
Your Name <you@example.com>
2022-03-01 13:40:00 +0100

    #28 fixed backend for getting user profile data

--- Haik/Haik/Pages/Profile.cshtml.cs
+++ Haik/Haik/Pages/Profile.cshtml.cs
@@ -1,16 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Haik.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Haik.Pages
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
+        public HaikDBContext context = null;
+
+        [BindProperty]
+        public ApplicationUser user { get; set; }
+        public ProfileModel(HaikDBContext context)
         {
+            this.context = context;
+        }
+
+        public IEnumerable<ApplicationUser> users { get; set; }
+        public IEnumerable<TripDb> trips { get; set; }
+        public async void OnGet()
+        {
+            users = await context.Users.ToListAsync();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            user = users.Where<ApplicationUser>(w => w.Id == userId).FirstOrDefault();
+            
         }
     }
 }
