b275b52a7ddd3f9243887febc5e0f91a74b592ea
Your Name <you@example.com>
2022-03-17 18:27:27 +0100

    #76 sorting from search now functional

--- Haik/Haik/Pages/Search.cshtml.cs
+++ Haik/Haik/Pages/Search.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Haik.Migrations;
+using Haik.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,29 @@ namespace Haik.Pages
 {
     public class SearchModel : PageModel
     {
-        public void OnGet()
+        private readonly HaikDBContext context;
+        public readonly List<TripDb> queriedTrips = new List<TripDb>();
+
+        public SearchModel(HaikDBContext context)
         {
+            this.context = context;
+        }
+        public async void OnGetAsync(string searchString = "")
+        {
+            var keywords = searchString.Split(" ");
+            foreach(var word in keywords)
+            {
+                queriedTrips.AddRange(context.Trips.Where<TripDb>(t => t.Name.Contains(word) || t.Location.Contains(word)).ToList());
+            }
+        }
+
+        public async Task OnPostAsync()
+        {
+            var keywords = Request.Form["search"].ToString().Split(" ");
+            foreach (var word in keywords)
+            {
+                queriedTrips.AddRange(context.Trips.Where<TripDb>(t => t.Name.Contains(word) || t.Location.Contains(word)).ToList());
+            }
         }
     }
 }
